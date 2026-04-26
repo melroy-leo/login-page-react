f9782df1b69eca7c5f15485b300ed81cffac3b6a
melroy <melroysesu@gmail.com>
2026-04-26 19:52:39 +0530

    fix api

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 
+const app = express();
+
 app.use(cors({
   origin: "*"
 }));
 
-const app = express();
-app.use(cors());
 app.use(express.json());
 
 const user = {
-    email: "test@gmail.com",
+  email: "test@gmail.com",
   password: "123456",
 };
 
-
-app.get("/login", (req, res) => {
+app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   if (email === user.email && password === user.password) {
-    return res.send({
+    return res.json({
       success: true,
       message: "Login successful",
     });
   } else {
-    return res.status(401).send({
+    return res.status(401).json({
       success: false,
       message: "Invalid email or password",
     });
